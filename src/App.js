a7560e8175226fc54b594d8ee2bc7fe9438a6185
Daniel Eduardo Almagro <danyeduard17@gmail.com>
2021-03-15 17:16:09 -0500

    Add: redux para mostrar la tabla.

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React from "react";
 import { Container } from "react-bootstrap";
 /* import "bootstrap/dist/css/bootstrap.min.css"; */
 import TablaReports from "./components/TablaReports";
+import { Provider } from "react-redux";
+import generateStore from "./redux/store";
 
 function App() {
   return (
     <Container>
-      <TablaReports />
+      <Provider store={generateStore()}>
+        <TablaReports />
+      </Provider>
     </Container>
   );
 }
--- src/components/TablaReports.jsx
+++ src/components/TablaReports.jsx
@@ -1,27 +1,38 @@
 import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Button, Container, Table } from "react-bootstrap";
+/* import { obtenerUsuarios } from "../redux/ducks"; */
 import Formulario from "./Formulario";
 
 const TablaReports = () => {
   useEffect(() => {
     document.title = "Usuarios";
   });
+
   const [show, setShow] = useState(true); // Mostrar y ocultar botones
 
   function mostrarFormulario() {
     setShow(!show);
   }
 
+  const usuarios = useSelector((store) => store.dataUsuarios.dataUsuarios);
+  console.log("usuarios", usuarios);
+  /* const dispatch = useDispatch(); */
+
   return (
     <Container>
       {show ? (
-        <Button onClick={() => mostrarFormulario()} type="button" variant="primary">
+        <Button
+          onClick={() => mostrarFormulario()}
+          type="button"
+          variant="primary"
+        >
           Agregar
         </Button>
       ) : (
         <Formulario />
       )}
-      <h2 center>Registro de usuarios</h2>
+      <h2>Registro de usuarios</h2>
       <Table responsive striped bordered hover>
         <thead>
           <tr>
@@ -40,20 +51,15 @@ const TablaReports = () => {
             <td>3017498942</td>
             <td>dany@email.com</td>
           </tr>
-          <tr>
-            <td>1001835011</td>
-            <td>Daniel Eduardo</td>
-            <td>Calle 12 #26</td>
-            <td>3017498942</td>
-            <td>dany@email.com</td>
-          </tr>
-          <tr>
-            <td>1001835011</td>
-            <td>Daniel Eduardo</td>
-            <td>Calle 12 #26</td>
-            <td>3017498942</td>
-            <td>dany@email.com</td>
-          </tr>
+          {usuarios.map((item) => (
+              <tr key={item.id}>
+                <td>{item.id}</td>
+                <td>{item.name}</td>
+                <td>{item.address}</td>
+                <td>{item.phone}</td>
+                <td>{item.email}</td>
+              </tr>
+            ))}
         </tbody>
       </Table>
     </Container>
