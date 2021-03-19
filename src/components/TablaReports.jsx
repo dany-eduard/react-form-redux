928a16fca3c34edbd4601683ca4c77d50e44d61e
Daniel Eduardo Almagro <danyeduard17@gmail.com>
2021-03-19 15:52:33 -0500

    Update: Simplificado
    Ya no se muestra el componente Formulario dentro de este.

--- src/components/TablaReports.jsx
+++ src/components/TablaReports.jsx
@@ -1,54 +1,20 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Button, Table } from "react-bootstrap";
-import { obtenerUsuarios, mostrarOcultarFormulario } from "../redux/ducks";
-import BtnMostrarForm from "./BtnMostrarForm";
-import Formulario from "./Formulario";
+import { Table } from "react-bootstrap";
+import { obtenerUsuarios } from "../redux/ducks";
 
 const TablaReports = () => {
   const dispatch = useDispatch();
 
-  const mostrarFormulario = useSelector((store) => store.dataUsuarios.mostrarFormulario);
-  // console.log("🚀 ~ file: TablaReports.jsx ~ line 19 ~ TablaReports ~ mostrarFormulario", mostrarFormulario)
-  const [show, setShow] = useState(mostrarFormulario);
-  // console.log("🚀 ~ file: TablaReports.jsx ~ line 20 ~ TablaReports ~ show", show)
-
-  const botonMostrarFormulario = () => {
-    setShow(true);
-    dispatch(mostrarOcultarFormulario(true))
-  }
-  
-  // const propsButton = {
-  //   form: true,
-  //   text: "Agregar",
-  //   variant: "primary"
-  // }
-
   useEffect(() => {
-    document.title = "Usuarios";
     obtenerUsuarios(dispatch);
   }, []);
 
-
-
   const usuarios = useSelector((store) => store.dataUsuarios.dataUsuarios);
   // console.log("🚀 ~ file: TablaReports.jsx ~ line 35 ~ TablaReports ~ usuarios", usuarios)
-  
 
   return (
     <>
-      {!show ? (
-        <Button
-          onClick={() => botonMostrarFormulario()}
-          type="button"
-          variant="primary"
-        >
-          Agregar
-        </Button>
-        // <BtnMostrarForm propsButton={propsButton} /* onClick={() => } *//>
-      ) : (
-        <Formulario />
-      )}
       <h2>Registro de usuarios</h2>
       <Table responsive striped bordered hover>
         <thead>
