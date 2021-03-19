af021aec1c60a040add5f2fdf44c42955b4a3aa2
Daniel Eduardo Almagro <danyeduard17@gmail.com>
2021-03-19 15:54:34 -0500

    Update: Simplificado
    Solo se usa para mostrar el botón Agregar nuevo usuario en la Tabla y
    Cancelar en el formulario. Al ser un componente reutilizable, puede
    ejecutar funciones dentro del componente en el que sea llamado.

--- src/components/BtnMostrarForm.jsx
+++ src/components/BtnMostrarForm.jsx
@@ -1,27 +1,17 @@
-import React, { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { Button, Table } from "react-bootstrap";
-import { obtenerUsuarios, mostrarOcultarFormulario } from "../redux/ducks";
+import React from "react";
+import { Button } from "react-bootstrap";
 
 const BtnMostrarForm = ({ propsButton }) => {
-  const dispatch = useDispatch();
-  const mostrarFormulario = useSelector((store) => store.dataUsuarios.mostrarFormulario);
-  const [show, setShow] = useState(mostrarFormulario);
   const booleano = propsButton.form;
   console.log(
     "🚀 ~ file: BtnMostrarForm.jsx ~ line 11 ~ BtnMostrarForm ~ booleano",
     booleano
   );
 
-  const botonMostrarFormulario = (booleano) => {
-    setShow(booleano);
-    dispatch(mostrarOcultarFormulario(booleano));
-  };
-
   return (
     <>
       <Button
-        onClick={() => botonMostrarFormulario(booleano)}
+        onClick={() => propsButton.botonMostrarFormulario(booleano)}
         type="button"
         variant={propsButton.variant}>
         {propsButton.text}
